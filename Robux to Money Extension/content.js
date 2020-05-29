6fb96822e0b2af7ac6d3b45c4782966991ec4609
OnlyTwentyFourCharacters <59615096+OnlyTwentyFourCharacters@users.noreply.github.com>
2020-05-29 12:50:38 -0600

    Bug fixes

--- Robux to Money Extension/content.js
+++ Robux to Money Extension/content.js
@@ -1,44 +1,60 @@
+//conversion values
 var DEVEX = 0.0035;
 var CAD_CONVERT = 1.38;
 var GBP_CONVERT = 0.81;
 var CNY_CONVERT = 7.13;
 var INR_CONVERT = 75.43;
 
-var robuxElement = document.getElementById('nav-robux-amount');
-var initialValue = parseInt(robuxElement.innerHTML);
+var robuxElement;
+var initialValue;
 var newValue;
 var symbol;
 
-//perform display based on conversion rate
-chrome.storage.local.get(['currency'], function(result) {
-  if (result.currency == "USD")
-  {
-    newValue = initialValue * DEVEX;
-    symbol = "$";
-  }
-  else if (result.currency == "CAD")
-  {
-    newValue = initialValue * DEVEX * CAD_CONVERT;
-    symbol = "$";
-  }
-  else if (result.currency == "GBP")
-  {
-    newValue = initialValue * DEVEX * GBP_CONVERT;
-    symbol = "&#163;";
-  }
-  else if (result.currency == "CNY")
-  {
-    newValue = initialValue * DEVEX * CNY_CONVERT;
-    symbol = "&#165;";
-  }
-  else if (result.currency == "INR")
-  {
-    newValue = initialValue * DEVEX * INR_CONVERT;
-    symbol = "&#8377;";
-  }
+//run script when window loads
+window.addEventListener("onload", function(){
 
-  Display();
-});
+  //get value from webpage
+  robuxElement = document.getElementById('nav-robux-amount');
+  initialValue = parseInt(robuxElement.innerHTML);
+
+  //perform display based on conversion rate
+  chrome.storage.local.get(['currency'], function(result) {
+    if (result.currency == "USD")
+    {
+      newValue = initialValue * DEVEX;
+      symbol = "$";
+      Display();
+    }
+    else if (result.currency == "CAD")
+    {
+      newValue = initialValue * DEVEX * CAD_CONVERT;
+      symbol = "$";
+      Display();
+    }
+    else if (result.currency == "GBP")
+    {
+      newValue = initialValue * DEVEX * GBP_CONVERT;
+      symbol = "&#163;";
+      Display();
+    }
+    else if (result.currency == "CNY")
+    {
+      newValue = initialValue * DEVEX * CNY_CONVERT;
+      symbol = "&#165;";
+      Display();
+    }
+    else if (result.currency == "INR")
+    {
+      newValue = initialValue * DEVEX * INR_CONVERT;
+      symbol = "&#8377;";
+      Display();
+    }
+    //if it reaches here, no currency is saved
+    //and the original robux value will stay
+
+  });
+
+})
 
 //Display final value
 function Display(){
